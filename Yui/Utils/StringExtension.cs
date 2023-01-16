005b23d0d9d2343b6d16bf74d6f9a19749907e4c
Velkha <velkapb99@gmail.com>
2023-01-16 12:16:23 +0100

    suisidio x2

---
+++ Yui/Utils/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace InteractionFramework.Utils
+{
+	public class StringExtension
+	{
+
+		/// <summary>
+		/// Checkea si el string en cuestion cumple el regex otorgado
+		/// </summary>
+		/// <param name="str">String a revisar</param>
+		/// <param name="pattern">Patron regex</param>
+		/// <returns></returns>
+		public static bool RegexMatch(String str, String pattern)
+		{
+			return Regex.Match(str, pattern);
+		}
+
+
+
+	}
+}
