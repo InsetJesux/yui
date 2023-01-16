03a2b6a22a29990771d0a6eeec9e9c1636ab5acd
Velkha <velkapb99@gmail.com>
2023-01-16 12:18:20 +0100

    socorro

--- Yui/InteractionHandler.cs
+++ Yui/InteractionHandler.cs
@@ -6,7 +6,7 @@ using System;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace Yui
+namespace InteractionFramework
 {
     public class InteractionHandler
     {
--- Yui/Program.cs
+++ Yui/Program.cs
@@ -7,7 +7,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Yui
+namespace InteractionFramework
 {
     public class Program
     {
