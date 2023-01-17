2a6f00f5184dfbfadd8e73fdd5b09479ea517582
InsetJesux <insetjesux@gmail.com>
2023-01-17 09:53:23 +0100

    Reorganizar clase principal del bot

---
+++ Yui/Yui.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Yui
+{
+    public class Yui
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceProvider _services;
+        private readonly DiscordSocketConfig _socketConfig;
+
+        public Yui(IConfiguration configuration = null, IServiceProvider services = null, DiscordSocketConfig socketConfig = null)
+        {
+            _configuration = configuration ?? BuildConfiguration();
+            _services = services ?? BuildServices();
+            _socketConfig = socketConfig ?? BuildSocketConfig();
+        }
+
+        public async Task RunAsync()
+        {
+            var client = _services.GetRequiredService<DiscordSocketClient>();
+
+            client.Log += LogAsync;
+
+            // Here we can initialize the service that will register and execute our commands
+            await _services.GetRequiredService<InteractionHandler>()
+                .InitializeAsync();
+
+            // Bot token can be provided from the Configuration object we set up earlier
+            await client.LoginAsync(TokenType.Bot, _configuration["token"]);
+            await client.StartAsync();
+
+            // Never quit the program until manually forced to.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "DC_")
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+        }
+
+        private IServiceProvider BuildServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_configuration)
+                .AddSingleton(_socketConfig)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>()
+                .BuildServiceProvider();
+        }
+
+        private DiscordSocketConfig BuildSocketConfig()
+        {
+            return new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+                AlwaysDownloadUsers = true,
+            };
+        }
+
+        private async Task LogAsync(LogMessage message)
+            => Console.WriteLine(message.ToString());
+
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
