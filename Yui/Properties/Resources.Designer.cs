c7b13d768e4c2925e359793c98472413d8a9f787
InsetJesux <insetjesux@gmail.com>
2023-01-17 09:53:00 +0100

    Cambio del namespace por defecto del proyecto

--- Yui/Properties/Resources.Designer.cs
+++ Yui/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Yui.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("InteractionFramework.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Yui.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
